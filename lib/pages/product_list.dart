514ba8dc20e61d206fd549c69be2eaacdf09505f
Ali-Shan11223 <jonah2114@gmail.com>
2023-08-29 14:33:41 +0500

    responsiveness done

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -91,13 +91,13 @@ class _ProductListState extends State<ProductList> {
         ),
         Expanded(
           child: LayoutBuilder(builder: (context, constraints) {
-            if (constraints.maxWidth > 650) {
+            if (constraints.maxWidth > 1080) {
               return GridView.builder(
                   physics: const BouncingScrollPhysics(),
                   itemCount: products.length,
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2,
-                    childAspectRatio: 2,
+                    childAspectRatio: 1.75,
                   ),
                   itemBuilder: (context, index) {
                     final item = products[index];
